198f87da2abfce015460b0d8e06725a863a7a570
unknown <mesi.mathi@gmail.com>
2021-03-05 16:03:47 +0100

    did a simple function to switch between temp unit

--- elements.js
+++ elements.js
@@ -3,5 +3,9 @@ let weatherIcon = document.querySelector(".loaction-icon");
 let temperatureValue = document.querySelector(".temperature-value");
 let temperatureUnit = document.querySelector(".temperature-unit");
 let description = document.querySelector(".temperature-description");
+let tempCont = document.querySelector(".degree-container");
 
-export { locationTitle, weatherIcon, temperatureValue, temperatureUnit, description };
+
+
+
+export { locationTitle, weatherIcon, temperatureValue, temperatureUnit, description, tempCont };
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//import {locationTitle, weatherIcon, temperatureValue, temperatureUnit, description } from "/elements.js";
 import * as elements from "/elements.js";
+let cels = true;
+let location = "Las Vegas";
 
-console.log(elements.locationTitle);
+
 window.addEventListener('load', ()=> {
     let long;
     let lat;
-    let berlin = "Berlin";
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -14,7 +14,7 @@ window.addEventListener('load', ()=> {
 
             //const proxy = "https://cors-anywhere.herokuapp.com/";
 
-            const api = `https://api.weatherapi.com/v1/current.json?key=4b35052eaeae47e3a5e112630210503&q=${lat},${long}&aqi=no`;
+            const api = `https://api.weatherapi.com/v1/current.json?key=4b35052eaeae47e3a5e112630210503&q=${location}&aqi=no`;
 
             fetch(api)
                 .then(response => {
@@ -22,8 +22,29 @@ window.addEventListener('load', ()=> {
                 })
                 .then(data => {
                     console.log(data);
+
+                    elements.temperatureValue.innerHTML = data.current.temp_c;
+                    elements.temperatureUnit.innerHTML = "°C";
+                    elements.locationTitle.innerHTML = data.location.name;
+                    elements.description.innerHTML = data.current.condition.text;
+                    elements.weatherIcon.src = data.current.condition.icon;
+
+                    elements.tempCont.addEventListener("click", () => {
+                        if (cels) {
+                            elements.temperatureUnit.innerHTML = "°C";
+                            elements.temperatureValue.innerHTML = data.current.temp_c;
+                            cels = false;
+                    
+                        }
+                        else if (!cels){
+                            elements.temperatureUnit.innerHTML = "F";
+                            elements.temperatureValue.innerHTML = data.current.temp_f;
+                            cels = true;
+                        }
+                    })
                 })
             
         })
     }
 });
+
